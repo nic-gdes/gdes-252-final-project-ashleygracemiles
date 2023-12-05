049ded939e0b0a981430210deaeafe3ce2774783
Ashley Miles <agmiles@nic.edu>
2023-12-05 15:49:28 -0800

    addition of calc page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,23 @@ function toggleMenu() {
 }
 
 document.querySelector('.close-button').addEventListener('click', function () {
-    console.log("Close button clicked");
     var menu = document.getElementById('nav-menu');
     menu.classList.remove('open');
 });
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function feetToMiles(feet) {
+    return feet / 5280;
+}
+
+function inchesToCentimeters(inches) {
+    return inches * 2.54;
+}
+
+function performConversion() {
+    // get the input value
+    var inputValue = document.getElementById
+}
